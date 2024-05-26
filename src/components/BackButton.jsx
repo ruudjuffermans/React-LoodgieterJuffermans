23dca7e9c68bbe6a6564487641ec1c06b376f22c
⚙️ Ruud Juffermans <info@2cluster.com>
2024-05-26 17:46:33 +0200

    small change to trigger workflow

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BackButton = () => {
   return (
     <div sx={{postion: "absolute", left: "20px", top: "20px", backgroundColor:"red", width: "50px", height: "50px", borderRadius: "50%"}}>
-123
+12345
 
     </div>
   )
